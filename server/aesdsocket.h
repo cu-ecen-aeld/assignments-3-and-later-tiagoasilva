f9275359dc14dbdadc7aaf7bc940ba923b1b5044
Tiago Silva <tiagoas@motorola.com>
2026-04-20 16:51:15 -0300

    Fixing port on aesdsocket
    Fixing port on aesdsocket

--- server/aesdsocket.h
+++ server/aesdsocket.h
@@ -13,7 +13,7 @@
 #include <arpa/inet.h>
 #include <sys/stat.h>
 
-#define PORT "9001" // the port client will be connecting to 
+#define PORT "9000" // the port client will be connecting to 
 #define MAXDATASIZE 50000 // max number of bytes we can get at once
 #define BUFFER_FILE "/var/tmp/aesdsocketdata" // the file used to write received content
 #define BACKLOG 1000 //The number of pending connections allowed before refusing
